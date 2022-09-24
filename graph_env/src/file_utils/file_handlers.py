f2c29803896c7f6d124cefb8037a2e3e51901b59
Gabriel Ruas <gabrielamaralruas@gmail.com>
2022-09-24 17:35:02 -0300

    +função de ler arquivo

---
+++ graph_env/src/file_utils/file_handlers.py
@@ -0,0 +1,28 @@
+
+
+
+from typing import List,Tuple
+from os import path, getcwd
+
+
+def ler_arquivo(caminho:str) -> None:
+    numero_vertices:int;
+    arestas:List[Tuple(int)] = [];
+    
+    with open(path.join(getcwd(),caminho), 'r') as file:
+        linhas = file.readlines();
+        numero_vertices = int(linhas[0]);
+        for linha in linhas[1:]:
+            numeros_na_linha = linha.split(' '); #separa por backspace
+            aresta = (int(numeros_na_linha[0]),int(numeros_na_linha[1]));
+            arestas.append(aresta); #adiciona tupla da aresta na lista de arestas            
+    
+    return [numero_vertices, arestas];
+
+
+caminho = 'test.txt';
+
+numerozin, arestaszinhas = ler_arquivo(caminho);
+
+print(f"Vértices:{numerozin}");
+print(f"Arestas:{arestaszinhas}");
