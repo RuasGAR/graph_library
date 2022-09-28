f43e5fd3fb92ae93772618054cb795cc4508c26f
Gabriel Ruas <gabrielamaralruas@gmail.com>
2022-09-28 18:43:08 -0300

    vetor de adjacencia imprimindo corretamente

--- graph_env/src/data_structures/adjacency_vector.py
+++ graph_env/src/data_structures/adjacency_vector.py
@@ -1,3 +1,4 @@
+from cgi import test
 from initial_element import ElementoInicialVetorAdj
 
 class VetorAdj:
@@ -5,7 +6,7 @@ class VetorAdj:
     def __init__(self,num_vertices,arestas):
 
         #Percorrer todas as arestas, criando um elemento inicial para cada vértice
-        self.container = [ElementoInicialVetorAdj(x) for x in range(num_vertices)]
+        self.container = [ElementoInicialVetorAdj(x) for x in range(1,num_vertices+1)]
 
         # Agora, vamos preencher os vizinhos de acordo com as arestas
         # Arestas: [(1, 2), (2, 5), (5, 3), (4, 5), (1, 5)]
@@ -16,12 +17,23 @@ class VetorAdj:
                 # Também temos de fazer o contrário: par[1] precisa saber que par[0] é seu vizinho
 
         for par in arestas:
-            self.container[par[0]-1].vetor_vizinhos.append(par[1]-1)
-            self.container[par[1]-1].vetor_vizinhos.append(par[0]-1) 
+            self.container[par[0]-1].vetor_vizinhos.append(par[1])
+            self.container[par[1]-1].vetor_vizinhos.append(par[0]) 
+        #print(self.container)
 
     def percorrer_vizinhos(self,valor_vertice:int):
-        return self.container[valor_vertice].vetor_vizinhos;
-        
-    def __repr__(self) -> str:
-        for v in self.container:
-            print(v);
+        if(valor_vertice > 0):
+            return self.container[valor_vertice-1].vetor_vizinhos;
+    
+    def imprimir(self):
+        for elemento in self.container:
+            print(elemento)
+
+# ==========================================================================================================================
+# Testes
+
+""" teste_vetor_adj = VetorAdj(5,[(1, 2), (2, 5), (5, 3), (4, 5), (1, 5)])
+teste_vetor_adj.imprimir()
+print("Vizinhos de 2: " + str(teste_vetor_adj.percorrer_vizinhos(2)));
+print("Vizinhos de 5: " + str(teste_vetor_adj.percorrer_vizinhos(5))); """
+
