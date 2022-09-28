99b189b55a33c5b47de039379bde0c8f78af6300
Gabriel Ruas <gabrielamaralruas@gmail.com>
2022-09-28 18:33:01 -0300

    finalizando os elementos iniciais dos vetores adjacentes

--- graph_env/src/data_structures/initial_element.py
+++ graph_env/src/data_structures/initial_element.py
@@ -1,7 +1,18 @@
 class ElementoInicialVetorAdj:
     
-    def __init__(self,valor) -> None:
+    def __init__(self, valor) -> None:
         self.valor = valor
         self.vetor_vizinhos = []
 
-        
+    def __str__(self) -> str:
+        string = f"Nó {self.valor}: -> " 
+        string += f"Vetor de Vizinhos:{str(self.vetor_vizinhos)}\n"
+        string += "================================\n";
+        return string
+
+# Teste
+""" elemento = ElementoInicialVetorAdj(1);
+for i in range(2,6):
+    elemento.vetor_vizinhos.append(i)
+
+print(elemento); """
