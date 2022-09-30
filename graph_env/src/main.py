12222e1b9ca5756de38fa6ed9691dd20691d2244
Gabriel Ruas <gabrielamaralruas@gmail.com>
2022-09-29 21:41:27 -0300

    + CRIANDO MASTER

--- graph_env/src/main.py
+++ graph_env/src/main.py
@@ -1,10 +1,10 @@
 from data_structures.adjacency_vector import VetorAdj
 from file_utils.file_handlers import ler_arquivo
-from os import getcwd, path
+""" from os import getcwd, path
 import data_structures
 import searches
 import file_utils
-
+ """
 def __main__():
     pass
 __main__();
