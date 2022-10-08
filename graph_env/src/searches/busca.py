5b0a1f94332a92d4dce80abf49ee090e4aac6d1d
Gabriel Ruas <gabrielamaralruas@gmail.com>
2022-10-08 17:30:21 -0300

    CERTIFICAÇAO: buscas funcionando corretmente em Matrizes de Adjacencia

--- graph_env/src/searches/busca.py
+++ graph_env/src/searches/busca.py
@@ -1,4 +1,6 @@
+
 import sys
+from typing import List, Union
     # caution: path[0] is reserved for script path (or '' in REPL)
 sys.path.insert(1,'/home/ruasgar/Bureau/trabalho_grafos/graph_env/src');
 
@@ -10,16 +12,17 @@ from collections import deque
 
 class Busca:
 
-    def __init__(self,grafo) -> None:
-        self.grafo = grafo;
-        self.vertices = [];
+    def __init__(self,grafo:Union[MatrizAdj, VetorAdj]) -> None:
+        self.grafo:Union[MatrizAdj, VetorAdj] = grafo;
+        self.vertices:List[Vertice] = [];
        
         if(isinstance(grafo, VetorAdj)):
             self.vertices = [Vertice(x.valor) for x in grafo.container] # Quantidade de elementos no vetor container é n 
         elif(isinstance(grafo,MatrizAdj)):
             self.vertices = [Vertice(x+1) for x in range(len(grafo.matriz))] # Quantidade de linhas da matriz é n
 
-    def bfs(self, vertice_s, retorna_todos=False):
+    def bfs(self, vertice_s:Vertice) -> List[Vertice]:
+        
         # Desmarcar todos os vértices
         # Por natureza, já são desmarcados, mas alterando por referência, é sempre bom rever
 
@@ -35,71 +38,92 @@ class Busca:
         self.vertices[vertice_s.valor-1].marcador = True;
         #vertice_s.nivel = 0; desnecessário, mas vou deixar o comentário pra lembrar
         fila.append(vertice_s);
+        
         # Loop
         while (len(fila) != 0):      
-            #print(fila);  
+        
             v = fila.pop();
-            #to do : substituir (w-1) por variavel
+            
             for w in (self.grafo.percorrer_vizinhos(v.valor)):
-                if (self.vertices[w-1].marcador == False):
-                    self.vertices[w-1].marcador = True;
-                    self.vertices[w-1].nivel = v.nivel + 1;
-                    self.vertices[w-1].pai = v.valor; #o ideal seria o endereço de v, mas...
-                    fila.insert(0,self.vertices[w-1])
+
+                vizinho_no_vetor:Vertice = self.vertices[w-1];
+                
+                if (vizinho_no_vetor.marcador == False):
+                    vizinho_no_vetor.marcador = True;
+                    vizinho_no_vetor.nivel = v.nivel + 1;
+                    vizinho_no_vetor.pai = v.valor; #o ideal seria o endereço de v, mas...
+                    
+                    fila.insert(0,vizinho_no_vetor)
 
         return list(filter(lambda x: x.marcador == True, self.vertices));
 
 
-    def dfs(self, vertice_s):
+    def dfs(self, vertice_s:Vertice) -> List[Vertice]:
         #dermarcando todos os vértices
         for no in self.vertices:
             no.marcador = False;
             no.pai = None;
             no.nivel = 0;
+        
         # pegar no vetor de vértices o vértice s
         pilha = deque()
+        
         pilha.append(self.vertices[vertice_s.valor-1])
+        
         while len(pilha) != 0 :
-            #print(list(pilha))
-            u = pilha.popleft()
+            
+            u:Vertice = pilha.popleft()
+            
             if u.marcador == False :
                 # O elemento que está na posição relativa do vértice retirado dentro de vetor de vértices (self.vertices)
                 u_no_vetor = self.vertices[u.valor-1]
-                u_no_vetor.marcador = True
                 # Mesma ideia: precisamos marcar o vértice no vetor self.vertices; por isso não pegamos somente o vérice U isolado
-                for vizinho in self.grafo.percorrer_vizinhos(u_no_vetor.valor):
+                u_no_vetor.marcador = True
+                
+                # Vamos pegar os vizinhos e se for uma matriz de adj, precisamos inverter sua ordem:
+                vizinhos_de_u:List[int] = self.grafo.percorrer_vizinhos(u_no_vetor.valor)
+                if(isinstance(self.grafo,MatrizAdj)):
+                    vizinhos_de_u.reverse();
+                
+                # percorremos os vizinhos
+                for vizinho in vizinhos_de_u:
                     vizinho_no_vetor = self.vertices[vizinho-1]
-                    # O if abaixo seria possível fazer com checagem da marcação;
-                    # Porém, como ele só é necessário por conta da atualização do pai e do nível nesse local...
-                    # Optamos por fazer duas checagens em O(1) pela legibilidade e entendimento do processo
+
                     if(vizinho_no_vetor.marcador == False): 
                         vizinho_no_vetor.pai = u_no_vetor.valor
                         vizinho_no_vetor.nivel = u_no_vetor.nivel + 1
-                    pilha.append(vizinho_no_vetor);
+                    pilha.insert(0,vizinho_no_vetor);
+
         return list(filter(lambda x: x.marcador == True, self.vertices));
 
-n, arestas = ler_arquivo("graph_env/src/test.txt")
+n, arestas = ler_arquivo("graph_env/src/8_vertex_graph.txt")
 
 
 """ Testes para matriz de adjacência """
 
-grafo_em_matriz = MatrizAdj(n);
-grafo_em_matriz.inserir_arestas(arestas);
+#grafo_em_matriz = MatrizAdj(n);
+#grafo_em_matriz.inserir_arestas(arestas);
 #grafo_em_matriz.mostra_matriz();
 #print(grafo_em_matriz.percorrer_vizinhos(3));
-matriz_busca = Busca(grafo_em_matriz);
-#print(matriz_busca.vertices);
+#matriz_busca = Busca(grafo_em_matriz);
 
+# Teste na BFS
 #print(matriz_busca.bfs(Vertice(5)),'\n')
-#print(matriz_busca.dfs(Vertice(5)),'\n')
+#print(matriz_busca.bfs(Vertice(1)),'\n')
+
+# Teste na DFS
+#print(matriz_busca.dfs(Vertice(4)),'\n')
+#print(matriz_busca.dfs(Vertice(2)),'\n')
+
+
 
 """ Testes para vetor de adjacências """
 
-grafo_em_vetor = VetorAdj(n, arestas);
+""" grafo_em_vetor = VetorAdj(n, arestas);
 vetor_adj_busca = Busca(grafo_em_vetor);
-#print(str(vetor_adj_busca.vertices)[1:-1].replace(', ','')); #print chatinho pra kct
-#print(vetor_adj_busca.bfs(Vertice(5))) 
-#print(vetor_adj_busca.dfs(Vertice(5))) 
-#vetor_adj_busca.dfs(Vertice(1))
+print(str(vetor_adj_busca.vertices)[1:-1].replace(', ',''));
+print(vetor_adj_busca.bfs(Vertice(5))) 
+print(vetor_adj_busca.dfs(Vertice(5))) 
+vetor_adj_busca.dfs(Vertice(1)) """
     
         
