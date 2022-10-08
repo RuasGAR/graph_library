21df50d2f7a545840402806392c6b73118627b10
Gabriel Ruas <gabrielamaralruas@gmail.com>
2022-10-08 19:44:23 -0300

    ### CONSERTO IMPORTANTE NA DISTANCIA E DIAMETRO

--- graph_env/src/extra_functions/functions.py
+++ graph_env/src/extra_functions/functions.py
@@ -1,8 +1,10 @@
-from typing import List, Union
+from typing import List, Mapping, Union
 
 import sys
 
 import numpy as np
+from math import inf, isinf
+
     # caution: path[0] is reserved for script path (or '' in REPL)
 sys.path.insert(1,'/home/ruasgar/Bureau/trabalho_grafos/graph_env/src');
 
@@ -26,14 +28,28 @@ def grau_medio(grafo:Union[MatrizAdj, VetorAdj]) -> np.float32:
 def grau_mediana(grafo:Union[MatrizAdj, VetorAdj]) -> int:
     return grafo.grau_mediana();
 
-def distancia(grafo:Union[MatrizAdj, VetorAdj], origem:Vertice, destino:Vertice) -> Union[Vertice, None]:
+def distancia(grafo:Union[MatrizAdj, VetorAdj], origem:int, destino:int) -> Union[int,float]:
 
-    explorados:List[Vertice] = Busca(grafo).bfs(origem);
+    if(origem == destino):
+        return inf;
+
+    explorados:List[Vertice] = Busca(grafo).bfs(Vertice(origem));
+
+    # Verificar se o vertice de destino está nos alcançaveis a partir da origem.
+    # Não podemmos verificar pela estrutura do vértice, e sim pelo seu valor.
+    # Para casos de desconexão, retornamos infinito 
     
-    if(explorados[origem.valor-1] and explorados[destino.valor-1]):
-        return (explorados[destino.valor-1].nivel - explorados[origem.valor-1].nivel)
+    nivel_vertice_final:int = 0;
+
+    for vertice in explorados:
+        if(vertice.valor == destino):
+            nivel_vertice_final = vertice.nivel;
     
-    return None;
+    # Checamos se o final foi encontrado; se não, os dois vértices não tem conectividade, e portanto, sua distância é infinita
+    if(nivel_vertice_final != 0):
+        return nivel_vertice_final;
+    
+    return inf;
 
 def diametro(grafo:Union[MatrizAdj, VetorAdj]) -> int:
 
@@ -42,19 +58,19 @@ def diametro(grafo:Union[MatrizAdj, VetorAdj]) -> int:
 
     diametro:int = 0;
     for i in range(grafo.num_vertices):
-        for j in range(grafo.num_vertices):
-            nova_distancia = distancia(grafo, Vertice(i), Vertice(j));
-            if(nova_distancia > diametro):
+        for j in range(i+1,grafo.num_vertices):
+            nova_distancia = distancia(grafo, i+1,j);
+            if(isinf(nova_distancia) == False and nova_distancia > diametro):
                 diametro = nova_distancia;
     return diametro
 
-def componentes_conexas(grafo:Union[MatrizAdj, VetorAdj]) -> List[object[List[Vertice],int]]:
+def componentes_conexas(grafo:Union[MatrizAdj, VetorAdj]) -> List[Mapping[List[Vertice],int]]:
     
     # Estruturas auxiliares
         # Lista de componentes conexas
         # Dicionário relacionando cada uma das componentes ao seu respectivo tamanho
 
-    componentes_conexas:List[object[List[Vertice],int]] = [];
+    componentes_conexas:List[Mapping[List[Vertice],int]] = [];
 
     # Instância da busca, para que tenhamos os vértices com as marcações e possamos, intuitivamente, realizar as buscas
     busca_em_grafo:Busca = Busca(grafo);
@@ -93,8 +109,10 @@ def componentes_conexas(grafo:Union[MatrizAdj, VetorAdj]) -> List[object[List[Ve
 
     return componentes_conexas;
 
-n, arestas = ler_arquivo("graph_env/src/8_vertex_graph.txt")
 
+###### TEST ZONE !!!! ########################################
+
+n, arestas = ler_arquivo("graph_env/src/8_vertex_graph.txt")
 
 # Testes para matriz de adjacência
 
@@ -104,18 +122,21 @@ n, arestas = ler_arquivo("graph_env/src/8_vertex_graph.txt")
 print(f"Min: {grau_minimo(grafo_em_matriz)}\n");
 print(f"Médio: {grau_medio(grafo_em_matriz)}\n");
 print(f"Mediana: {grau_mediana(grafo_em_matriz)}\n");
-print(f"Distância entre 4 e 1: {distancia(grafo_em_matriz,Vertice(4),Vertice(1))}")
-print(f"Diâmetro: {diametro(grafo_em_matriz)}"); """
-#print(f"Componentes Conexas: {componentes_conexas(grafo_em_matriz)}");
+print(f"Distância entre 4 e 1: {distancia(grafo_em_matriz,4,1)}")
+#print(f"Componentes Conexas: {componentes_conexas(grafo_em_matriz)}"); """
+#print(f"Diâmetro: {diametro(grafo_em_matriz)}\n"); 
+#print(f"Distância entre 8 e 4: {distancia(grafo_em_matriz,8,4)}")
+
 
 # Testes para vetor de adjacências
 
-""" grafo_em_vetor = VetorAdj(n, arestas);
-#print(str(vetor_adj_busca.vertices)[1:-1].replace(', ','')); #print chatinho pra kct
-print(f"\nMax: {grau_maximo(grafo_em_vetor)}\n");
-print(f"Min: {grau_minimo(grafo_em_vetor)}\n");
-print(f"Médio: {grau_medio(grafo_em_vetor)}\n");
-print(f"Mediana: {grau_mediana(grafo_em_vetor)}\n");
-print(f"Distância entre 3 e 1: {distancia(grafo_em_vetor,Vertice(3),Vertice(1))}\n")
-print(f"Diâmetro: {diametro(grafo_em_vetor)}") """
+grafo_em_vetor = VetorAdj(n, arestas);
+#print(str(vetor_adj_busca.vertices)[1:-1].replace(', ',''));
+#print(f"\nMax: {grau_maximo(grafo_em_vetor)}\n");
+#print(f"Min: {grau_minimo(grafo_em_vetor)}\n");
+#print(f"Médio: {grau_medio(grafo_em_vetor)}\n");
+#print(f"Mediana: {grau_mediana(grafo_em_vetor)}\n");
+#print(f"Distância entre 9 e 11: {distancia(grafo_em_vetor,9,11)}\n")
+#print(f"Diâmetro: {diametro(grafo_em_vetor)}\n")
+#print(f"Componentes Conexas: {componentes_conexas(grafo_em_vetor)}");
 
