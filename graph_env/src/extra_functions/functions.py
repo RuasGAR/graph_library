01cac6fe01f224dbde14ff46ae1758de616f2b97
Gabriel Ruas <gabrielamaralruas@gmail.com>
2022-10-08 18:15:40 -0300

    + annotations

--- graph_env/src/extra_functions/functions.py
+++ graph_env/src/extra_functions/functions.py
@@ -1,4 +1,8 @@
+from typing import List, Union
+
 import sys
+
+import numpy as np
     # caution: path[0] is reserved for script path (or '' in REPL)
 sys.path.insert(1,'/home/ruasgar/Bureau/trabalho_grafos/graph_env/src');
 
@@ -10,33 +14,33 @@ from file_utils.file_handlers import ler_arquivo
 from copy import deepcopy
 
 
-def grau_maximo(grafo):
+def grau_maximo(grafo:Union[MatrizAdj, VetorAdj]) -> int:
     return grafo.grau_maximo();
 
-def grau_minimo(grafo):
+def grau_minimo(grafo:Union[MatrizAdj, VetorAdj]) -> int:
     return grafo.grau_minimo();
 
-def grau_medio(grafo):
+def grau_medio(grafo:Union[MatrizAdj, VetorAdj]) -> np.float32:
     return grafo.grau_medio();
 
-def grau_mediana(grafo):
+def grau_mediana(grafo:Union[MatrizAdj, VetorAdj]) -> int:
     return grafo.grau_mediana();
 
-def distancia(grafo, origem, destino):
+def distancia(grafo:Union[MatrizAdj, VetorAdj], origem:Vertice, destino:Vertice) -> Union[Vertice, None]:
 
-    explorados = Busca(grafo).bfs(origem);
+    explorados:List[Vertice] = Busca(grafo).bfs(origem);
     
     if(explorados[origem.valor-1] and explorados[destino.valor-1]):
         return (explorados[destino.valor-1].nivel - explorados[origem.valor-1].nivel)
     
     return None;
 
-def diametro(grafo):
+def diametro(grafo:Union[MatrizAdj, VetorAdj]) -> int:
 
     # Tem que chamar a função de distância para cada combinação de vértices
     # Dá pra ser feito com dois loops. Atualizar a maior se for necessária.
 
-    diametro = 0;
+    diametro:int = 0;
     for i in range(grafo.num_vertices):
         for j in range(grafo.num_vertices):
             nova_distancia = distancia(grafo, Vertice(i), Vertice(j));
@@ -44,16 +48,16 @@ def diametro(grafo):
                 diametro = nova_distancia;
     return diametro
 
-def componentes_conexas(grafo):
+def componentes_conexas(grafo:Union[MatrizAdj, VetorAdj]) -> List[object[List[Vertice],int]]:
     
     # Estruturas auxiliares
         # Lista de componentes conexas
         # Dicionário relacionando cada uma das componentes ao seu respectivo tamanho
 
-    componentes_conexas = [];
+    componentes_conexas:List[object[List[Vertice],int]] = [];
 
     # Instância da busca, para que tenhamos os vértices com as marcações e possamos, intuitivamente, realizar as buscas
-    busca_em_grafo = Busca(grafo);
+    busca_em_grafo:Busca = Busca(grafo);
     
     """ primeira_componente = busca_em_grafo.bfs(busca_em_grafo.vertices[0]);
     
@@ -66,15 +70,15 @@ def componentes_conexas(grafo):
     # Porém, removemos as referências para os vértices "originais" e não precisamos nos preocupar em alterá-los diretamente
     # Além disso, nesse formato, adotamos uma abordagem que leva o maior custo no início do algoritmo, e vai aliviando
     # espaço conforme as componentes forem descobertas
-    desmarcados = list(map(lambda v: v.valor,deepcopy(busca_em_grafo.vertices)));  
+    desmarcados:List[Vertice] = list(map(lambda v: v.valor,deepcopy(busca_em_grafo.vertices)));  
 
     while (len(desmarcados) > 0): # Checagem 0(1)
         
         # pega componente
-        componente = busca_em_grafo.bfs(Vertice(desmarcados[0]));    
+        componente:List[Vertice] = busca_em_grafo.bfs(Vertice(desmarcados[0]));    
         
         # Componentes serão armazenadas no formato deste objeto
-        componentes_conexas.append({ "valor":componente, "tamanho":len(componente)});
+        componentes_conexas.append({"valor":componente, "tamanho":len(componente)});
         
         # remove os vertices da cópia que forem marcados
         for vertice in componente: # c vertices
